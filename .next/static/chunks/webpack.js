3b7b3ccc98eb933dfd116e972ebf64577a1b6fa7
Kekray <kekray777@gmail.com>
2022-12-04 17:47:18 +0400

    Remove unnecesary folder

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b72af1c47eb94783"; }
+/******/ 		__webpack_require__.h = function() { return "606089a02dc89f12"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/navbar.js
+++ components/navbar.js
@@ -45,7 +45,22 @@ const Navbar = props => {
       zIndex={1}
       {...props}
     >
-      Navbar
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+      </Container>
     </Box>
   )
 }
+
+export default Navbar
